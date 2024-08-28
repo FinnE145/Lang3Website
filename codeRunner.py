62d190dcffd9cb3facc9aa19e1eff3ac5793685a
FinnE145 <finn.emmerson@renertschool.ca>
2024-08-28 09:20:11 -0600

    Code Runner: Updated path to executable, renamed executable

--- codeRunner.py
+++ codeRunner.py
@@ -4,7 +4,7 @@ from subprocess import Popen, PIPE
 #TODO: implement cformat (code sxhl)
 #from iformat import cformat
 
-binary = "/home/finn/flask1/CompiledSource/Lang3Source"
+binary = "/home/finn/Lang3Website/CompiledSource/Lang3"
 
 def runLang3(fp):
     p = Popen([binary, fp, "!d"], stdout=PIPE, stderr=PIPE)
